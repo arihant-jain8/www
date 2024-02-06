24a83ca18c9bb7482756a0739ff3770899372ab6
arihant-jain8 <aarihant2001@gmail.com>
2024-02-06 23:40:09 +0530

    completed js basics

---
+++ JS-Basics/operations.js
@@ -0,0 +1,28 @@
+// Math operations
+
+console.log(10 + 4);
+console.log(10 - 4);
+console.log(10 * 4);
+console.log(10 / 4);
+console.log(10 % 4);
+
+let result = 5;
+result = result + 10;
+result++;
+result--;
+
+result += 5;
+result -= 5;
+result *= 5;
+result /= 5;
+
+// String operations
+console.log('Ari' + ' ' + 'Theory');
+
+let userName = 'Ari';
+console.log(userName.length);
+console.log(userName.toUpperCase());
+
+let hobbies = ['Sports', 'Cooking'];
+console.log(hobbies.length);
+
