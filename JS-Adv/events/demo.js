615aae5d757feab333db57c16a8efa529c49c2c0
arihant-jain8 <aarihant2001@gmail.com>
2024-02-17 00:29:26 +0530

    added loops.js in JS-adv

--- JS-Adv/events/demo.js
+++ JS-Adv/events/demo.js
@@ -19,6 +19,16 @@ function updateRemainingChar(event){
     const remainingChars = maxAllowedChars - enteredTextLength;
 
     remainingCharsElement.textContent = remainingChars;
+
+    // does not removes it when more than 10 chars
+    if(remainingChars <= 10){
+        remainingCharsElement.classList.add('warning');
+        productNameEle.classList.add('warning');
+    }
+    
+    else{
+        
+    }
 }
 
 productNameEle.addEventListener('input', updateRemainingChar);
