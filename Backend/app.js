ab9ee13a185b5e5eeb1c7135c46a4f8e9f2dd529
arihant-jain8 <aarihant2001@gmail.com>
2024-03-12 00:15:10 +0530

    node and express

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,30 @@
-const http = require('http');
+// const http = require('http');
+const express = require('express');
+
+const app = express();
+
+app.get('/'){
+    
+}
+
+app.listen(6969);
+
 
 function handleReq(req, resp){
+    // status codes
     // 200 - req parsed successfully, a resp can be generated and sent 
     // 404 - client side error. requested resource/link was not found.
     // 401 - client side error. client(user) is not authorized to access the requested resource/link
     // 500 - server-side error. req was valid.
-    resp.statusCode = 200;
-    resp.end('<h1>BILLLIIIII 🐈‍⬛</h1>');
+
+    if(req.url === '/currenttime'){
+        resp.statusCode = 200;
+        resp.end('<h1>' + new Date().toISOString() + '</h1>');
+    }
+    else if(req.url === '/'){
+        resp.statusCode = 200;
+        resp.end('<h1>BILLLIIIII 🐈‍⬛</h1>');
+    }
 }
 
 const server = http.createServer(handleReq);
