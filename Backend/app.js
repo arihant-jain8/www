4b8cabf06cf7cbd2bb2f2cd8de42effbb2b64c3e
arihant-jain8 <aarihant2001@gmail.com>
2024-03-12 14:34:44 +0530

    completed express basics and added express more base files

--- Backend/app.js
+++ Backend/app.js
@@ -1,35 +1,84 @@
-// const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
 
 const app = express();
 
-app.get('/'){
+app.use(express.urlencoded({extended: false}));
+// incoming request data parser
+// parses the incoming data and convert into js object
+
+app.get('/', function(req, res){
+    // res.send('<h1>BILLIIII</h1>')
+    res.send('<form action = "/store-user" method = "POST"><label>Name</label><input type="text" name="uname"><button>Submit</button></form>')
+});
+
+app.get('/currenttime', function(req, res){
+    res.send('<h1>' + new Date().toISOString() + '</h1>');
+}); // localhost:6969/currenttime
+
+app.post('/store-user', function(req, res){
+    const uname = req.body.uname;
+
+    const filePath = path.join(__dirname, 'data', 'users.json');
+    // console.log(filePath);
+
+    const fileData = fs.readFileSync(filePath);
+    const existingUsers = JSON.parse(fileData); // converts text from json format to js obj or array 
+    // console.log(existingUsers);
+
+    existingUsers.push(uname);
+
+    fs.writeFileSync(filePath, JSON.stringify(existingUsers)); // stringify is reverse of parse (js obj or array -> json)
+
+    res.send(uname + ' uname stored successfully');
+});
+
+app.get('/users', function(req, res){
+    const filePath = path.join(__dirname, 'data', 'users.json');
+
+    const fileData = fs.readFileSync(filePath);
+    const usersArray = JSON.parse(fileData);
+
+    let responseData = '<ul>';
+
+    for(let user of usersArray){
+        responseData += '<li>' + user + '</li>';
+    }
+
+    responseData += '</ul';
     
-}
+    res.send(responseData);
+});
 
 app.listen(6969);
 
 
-function handleReq(req, resp){
-    // status codes
-    // 200 - req parsed successfully, a resp can be generated and sent 
-    // 404 - client side error. requested resource/link was not found.
-    // 401 - client side error. client(user) is not authorized to access the requested resource/link
-    // 500 - server-side error. req was valid.
 
-    if(req.url === '/currenttime'){
-        resp.statusCode = 200;
-        resp.end('<h1>' + new Date().toISOString() + '</h1>');
-    }
-    else if(req.url === '/'){
-        resp.statusCode = 200;
-        resp.end('<h1>BILLLIIIII 🐈‍⬛</h1>');
-    }
-}
+// with http
+// const http = require('http');
 
-const server = http.createServer(handleReq);
+// function handleReq(req, resp){
+//     // status codes
+//     // 200 - req parsed successfully, a resp can be generated and sent 
+//     // 404 - client side error. requested resource/link was not found.
+//     // 401 - client side error. client(user) is not authorized to access the requested resource/link
+//     // 500 - server-side error. req was valid.
 
-server.listen(3000);
+//     if(req.url === '/currenttime'){
+//         resp.statusCode = 200;
+//         resp.end('<h1>' + new Date().toISOString() + '</h1>');
+//     }
+//     else if(req.url === '/'){
+//         resp.statusCode = 200;
+//         resp.end('<h1>BILLLIIIII 🐈‍⬛</h1>');
+//     }
+// }
+
+// const server = http.createServer(handleReq);
+
+// server.listen(3000);
 
 // amazon.com => send a req to amazon.com
 // common port to send unencrypted req
